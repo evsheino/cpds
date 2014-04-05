97975da40585f36d5fe735266b5cafb4cfa2ec5e
SImtiaz <SImtiaz@users.noreply.github.com>
2014-04-05 13:24:23 +0200

    Added pot's description

--- HW2/Banket/Banket.java
+++ HW2/Banket/Banket.java
@@ -2,7 +2,7 @@ public class Banket {
 
     public static void main(String args[]) {    
         int servings = Integer.parseInt(args[0]);
-        Pot pot = new Pot(5, 2);
+        Pot pot = new Pot(5, 2);  // Pot (capacity, savages)
         Thread a = new Savage("alice", pot, servings);
         Thread b = new Savage("bob", pot, servings);
         Thread c = new Cook(pot);
