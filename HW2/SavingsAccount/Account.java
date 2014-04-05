04460b602bb584716413e3a64ec3819ba7bb3a3e
SImtiaz <SImtiaz@users.noreply.github.com>
2014-04-05 13:50:32 +0200

    Account: changed "return false" -> wait @withdraw
    Savings account: instead of returning false, we can wait till we have
    enough balance in the account

--- HW2/SavingsAccount/Account.java
+++ HW2/SavingsAccount/Account.java
@@ -5,16 +5,17 @@ public class Account {
     public Account() { }
 
     public synchronized boolean withdraw(int amount) {
-        if (balance-amount < 0) return false;
+        if (balance-amount < 0) wait();
         balance -= amount;
-        System.out.println(balance);
+        System.out.println("balance:" + balance);
         return true;
     }
 
     public synchronized boolean deposit(int amount) {
         if (amount <= 0) return false;
         balance += amount;
-        System.out.println(balance);
+        System.out.println("balance:" + balance);
+        notifyAll();
         return true;
     }
 }
